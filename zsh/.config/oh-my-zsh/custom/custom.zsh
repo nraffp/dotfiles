bcbb784306a3604bc4a4c927a8879670b63ec02e
Nick <Raffaele.nickp@gamil.com>
2021-05-14 02:46:58 +0900

    Did some stuff

---
+++ zsh/.config/oh-my-zsh/custom/custom.zsh
@@ -0,0 +1,19 @@
+# Functions
+
+function zload(){
+       source ~/.zshrc
+       source ~/.config/oh-my-zsh/custom/custom.zsh
+       exec zsh
+       ...
+}
+
+# Alias
+
+alias ls='exa -g --group-directories-first --icons'
+
+# Path
+
+
+# Plugins
+
+## Zsh Highlightings
